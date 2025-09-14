16b7f3b53a0d5ed4fc135dfe891fb33cb72159fb
Benson Gikonyo <muhiugikonyo@gmail.com>
2025-09-14 21:50:08 +0300

    added class StudentClass. Task 4

--- 0x04-TypeScript/task_1/js/main.js
+++ 0x04-TypeScript/task_1/js/main.js
@@ -1,2 +1,23 @@
 ;
 ;
+;
+function printTeacher(_a) {
+    var firstName = _a.firstName, lastName = _a.lastName;
+    return "".concat(firstName, ". ").concat(lastName);
+}
+;
+var StudentClass = /** @class */ (function () {
+    function StudentClass(firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    ;
+    StudentClass.prototype.workOnHomework = function () {
+        return "Currently working";
+    };
+    ;
+    StudentClass.prototype.displayName = function () {
+        return this.firstName;
+    };
+    return StudentClass;
+}());
--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -19,3 +19,30 @@ interface printTeacherFunction {
 function printTeacher({ firstName, lastName }: { firstName: string; lastName: string }): string {
     return `${firstName}. ${lastName}`;
 };
+
+interface StudentClassInterface {
+    workOnHomework():string;
+    displayName():string;
+}
+
+interface StudentConstructor {
+    new (firstName:string, lastName:string): StudentClassInterface
+}
+
+class StudentClass implements StudentClassInterface{
+    firstName:string;
+    lastName:string;
+
+    constructor (firstName:string, lastName:string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    };
+
+    workOnHomework():string {
+        return `Currently working`;
+    };
+
+    displayName():string {
+        return this.firstName;
+    }
+}
